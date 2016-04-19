43ae38737f08ac4ea849081eafe3139bb6db65c5
Aadesh <aadesh@ajackus.com>
2016-04-19 21:17:11 +0530

    contentSecurityPolicy issue fixed

--- webapp/app/models/package.js
+++ webapp/app/models/package.js
@@ -3,10 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	name: DS.attr('string'),
 	description: DS.attr('string'),
-	price: DS.attr('float'),
-	discount: DS.attr('float'),
+	price: DS.attr('number'),
+	discount: DS.attr('number'),
 	is_half_day_fasting_applicable: DS.attr('boolean'),
-	special_instructions: DS.attr('text'),
+	special_instructions: DS.attr('string'),
 	number_of_visits_required: DS.attr('number'),
 	is_profile_test: DS.attr('boolean'),
 });
