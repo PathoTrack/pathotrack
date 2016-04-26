0af7a79fc1671453c4279863ff9af83d8a5f11d9
Aadesh <aadesh@ajackus.com>
2016-04-26 20:06:27 +0530

    [webapp] search functionality done, edit, delete, add new icons added

--- webapp/app/controllers/staff/packages/index.js
+++ webapp/app/controllers/staff/packages/index.js
@@ -2,11 +2,18 @@ import Ember from 'ember';
 import config from '../../../config/environment';
 
 export default Ember.Controller.extend({
+	queryParams: ['search'],
+    search: null,
+    searchField: Ember.computed.oneWay('search'),
+    
     actions: {
         delete: function(pack) {
             pack.deleteRecord();
             pack.save();
             return false;
+        },
+        search: function() {
+          this.set('search', this.get('searchField'));
         }
     }
 });
--- webapp/app/routes/login.js
+++ webapp/app/routes/login.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
+import BaseModel from '../models/base-model';
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+	beforeModel: function(transition) {
+        this._super(transition);
+        BaseModel.reopenClass({
+            namespace: 'open' 
+        });
+    }
 });
